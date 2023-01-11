05dcac8a79689c6d8874a78680f458aa47b777c4
Hmmmm-Josh <j.nahlous22@gmail.com>
2023-01-11 20:24:30 +1100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,42 +6,41 @@ class Main:
 
   def __init__(self):
     self.parser = argparse.ArgumentParser(description='Process some HTTP log files.')
-    self.parser.add_argument('--lines', metavar='file', type=str, help='count the number of lines in a file')
-    self.parser.add_argument('--status_codes', metavar='file', type=str, help='count the number of status codes in a file')
-    self.parser.add_argument('--traffic', metavar='file', type=str, help='sum the traffic in bytes for all requests in a file')
-    self.parser.add_argument('--unique_clients', metavar='file', type=str, help='Count the number of unique IP adresses')
-    self.parser.add_argument('--most_active_clients', metavar='file', type=str, help='Calculate the top 3 most active IP addresses')
-    self.parser.add_argument('--most_visited_paths', metavar='file', type=str, help='Calculate the top 3 most visited URLs')
+    self.parser.add_argument(dest='file', type=str, help="The file to read")
+    
+    self.parser.add_argument('--lines', type=str, help='count the number of lines in the file')
+    self.parser.add_argument('--status_codes', type=str, help='count the number of status codes in the file')
+    self.parser.add_argument('--traffic', type=str, help='sum the traffic in bytes for all requests in the file')
+    self.parser.add_argument('--unique_clients', type=str, help='Count the number of unique IP adresses in the file')
+    self.parser.add_argument('--most_active_clients', type=str, help='Calculate the top 3 most active IP addresses in the file')
+    self.parser.add_argument('--most_visited_paths', type=str, help='Calculate the top 3 most visited URLs in file')
 
   def run(self):
     args = self.parser.parse_args()
     
-    if not args.lines or args.status_codes or args.traffic:
+    if not args.file and (args.lines or args.status_codes or args.traffic or args.unique_clients or args.most_active_clients or args.most_visited_paths):
       raise Exception('Please provide an argument!')
+
+    parser = HttpLogProcessor(args.file)
     
     if args.lines:
-      parser = HttpLogProcessor(args.lines)
-      print(f'Number of lines in {args.lines}: {parser.get_total_lines()}')
+      print(f'Number of lines in {args.file}: {parser.get_total_lines()}')
     
     if args.traffic:
-      parser = HttpLogProcessor(args.traffic)
-      print(f'Total traffic in bytes in {args.traffic}: {parser.get_total_traffic()}')
+      print(f'Total traffic in bytes in {args.file}: {parser.get_total_traffic()}')
     
     if args.status_codes:
-      parser = HttpLogProcessor(args.status_codes)
-      print(f'Number of status codes in {args.status_codes}: {parser.get_status_code_count()}')
+      print(f'Number of status codes in {args.file}: {parser.get_status_code_count()}')
 
     if args.unique_clients:
-      parser = HttpLogProcessor(args.status_codes)
-      print(f'Number of unique clients in {args.status_codes}: {parser.get_status_code_count()}')
+     
+      print(f'Number of unique clients in {args.file}: {parser.get_status_code_count()}')
       
     if args.most_active_clients:
-      parser = HttpLogProcessor(args.status_codes)
-      print(f'Number of most active clients in {args.status_codes}: {parser.get_status_code_count()}')
+      print(f'Number of most active clients in {args.file}: {parser.get_status_code_count()}')
       
     if args.most_visited_paths:
-      parser = HttpLogProcessor(args.status_codes)
-      print(f'Most  in {args.status_codes}: {parser.get_status_code_count()}')
+      print(f'Most  in {args.file}: {parser.get_status_code_count()}')
 
     
 if __name__ == "__main__":
